49f74971a34386f1657ba67c6adfa1d3e948a550
Matthias Holzinger <Matthias.Holzinger@catalysts.cc>
2019-05-28 16:20:28 +0200

    dice rolling in class

--- src/main/kotlin/DiceRolling.kt
+++ src/main/kotlin/DiceRolling.kt
@@ -1,24 +1,27 @@
 import kotlin.math.sign
 
-data class Problem(val id: Int, val target: Int)
+class DiceRolling {
+    data class Problem(val id: Int, val target: Int)
 
-data class Solution(val count: Int) {
-    override fun toString(): String {
-        return "$count\n"
+    data class Solution(val count: Int) {
+        override fun toString(): String {
+            return "$count\n"
+        }
+    }
+
+    fun readProblem(problemId: Int): Problem {
+        return Problem(problemId, readLine()!!.toInt())
+    }
+
+    fun solveProblem(problem: Problem): Solution {
+        return Solution(problem.target / 7 + (problem.target % 7).sign)
     }
 }
 
-fun readProblem(problemId: Int): Problem {
-    return Problem(problemId, readLine()!!.toInt())
-}
-
-fun solveProblem(problem: Problem): Solution {
-    return Solution(problem.target / 7 + (problem.target % 7).sign)
-}
-
 fun main() {
+    val diceRolling = DiceRolling()
     val n = readLine()!!.toInt()
-    (1..n).map(::readProblem)
-        .map(::solveProblem)
+    (1..n).map(diceRolling::readProblem)
+        .map(diceRolling::solveProblem)
         .onEach(::print)
 }
